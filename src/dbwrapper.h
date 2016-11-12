bf95540681eb38aa14ea0aaf689fcdecf3321fcc
Xinxi Wang <wangxinxi@me.com>
2016-11-12 05:53:58 +0000

    tests: test dbwrapper options compression and maxopenfiles

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -193,7 +193,7 @@ public:
      * @param[in] obfuscate   If true, store data obfuscated via simple XOR. If false, XOR
      *                        with a zero'd byte array.
      */
-    CDBWrapper(const boost::filesystem::path& path, size_t nCacheSize, bool fMemory = false, bool fWipe = false, bool obfuscate = false);
+    CDBWrapper(const boost::filesystem::path& path, size_t nCacheSize, bool fMemory = false, bool fWipe = false, bool obfuscate = false, bool compression = false, int maxOpenFiles = 64);
     ~CDBWrapper();
 
     template <typename K, typename V>
